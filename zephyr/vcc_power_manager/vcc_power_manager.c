4dedb381fdee9964e2872dfc60eb6d688932467f
Dmitry Maslennikov <dmitry.maslennikov@ringcentral.com>
2025-09-09 17:47:37 +0200

    tinkering with P0.13 pin to cut off 3.3V VCC pin

---
+++ zephyr/vcc_power_manager/vcc_power_manager.c
@@ -0,0 +1,96 @@
+/*
+ * VCC Power Manager for nice!nano v2
+ * Automatically controls P0.13 to turn off 3.3V VCC when on battery
+ */
+
+#include <zephyr/kernel.h>
+#include <zephyr/device.h>
+#include <zephyr/init.h>
+#include <zephyr/logging/log.h>
+#include <zephyr/drivers/gpio.h>
+#include <zephyr/usb/usb_device.h>
+
+LOG_MODULE_REGISTER(vcc_power_manager, CONFIG_ZMK_LOG_LEVEL);
+
+#ifdef CONFIG_ZMK_VCC_POWER_MANAGER
+
+static const struct gpio_dt_spec vcc_power_pin = GPIO_DT_SPEC_GET_BY_IDX(DT_PATH(zephyr_user), gpios, 0);
+static bool usb_powered = false;
+static bool vcc_enabled = true;
+
+static void set_vcc_power(bool enable) {
+    if (!device_is_ready(vcc_power_pin.port)) {
+        LOG_ERR("VCC power control GPIO not ready");
+        return;
+    }
+    
+    if (enable && !vcc_enabled) {
+        LOG_INF("Enabling VCC power (USB connected)");
+        gpio_pin_set_dt(&vcc_power_pin, 1);
+        vcc_enabled = true;
+        k_msleep(100); // Give components time to power up
+    } else if (!enable && vcc_enabled) {
+        LOG_INF("Disabling VCC power (battery mode)");
+        gpio_pin_set_dt(&vcc_power_pin, 0);
+        vcc_enabled = false;
+    }
+}
+
+static void usb_status_cb(enum usb_dc_status_code status, const uint8_t *param) {
+    switch (status) {
+        case USB_DC_CONNECTED:
+        case USB_DC_CONFIGURED:
+            if (!usb_powered) {
+                usb_powered = true;
+                set_vcc_power(true);
+            }
+            break;
+            
+        case USB_DC_DISCONNECTED:
+        case USB_DC_SUSPEND:
+            if (usb_powered) {
+                usb_powered = false;
+                set_vcc_power(false);
+            }
+            break;
+            
+        default:
+            break;
+    }
+}
+
+static int vcc_power_manager_init(void) {
+    LOG_INF("Initializing VCC power manager for nice!nano v2");
+    
+    if (!device_is_ready(vcc_power_pin.port)) {
+        LOG_ERR("VCC power control GPIO not ready");
+        return -ENODEV;
+    }
+    
+    // Configure P0.13 as output
+    int ret = gpio_pin_configure_dt(&vcc_power_pin, GPIO_OUTPUT_ACTIVE);
+    if (ret < 0) {
+        LOG_ERR("Failed to configure VCC power GPIO: %d", ret);
+        return ret;
+    }
+    
+    // Initially enable VCC power
+    gpio_pin_set_dt(&vcc_power_pin, 1);
+    vcc_enabled = true;
+    
+    // Register USB status callback
+    usb_register_status_callback(usb_status_cb);
+    
+    // Check initial USB state after a delay
+    k_msleep(200);
+    if (!usb_powered) {
+        LOG_INF("No USB detected at startup, disabling VCC power");
+        set_vcc_power(false);
+    }
+    
+    return 0;
+}
+
+SYS_INIT(vcc_power_manager_init, APPLICATION, CONFIG_APPLICATION_INIT_PRIORITY);
+
+#endif /* CONFIG_ZMK_VCC_POWER_MANAGER */
