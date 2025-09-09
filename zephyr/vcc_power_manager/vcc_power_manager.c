536abe24fc72aae0d9540e41eebb5f8704176fb3
Dmitry Maslennikov <dmitry.maslennikov@ringcentral.com>
2025-09-09 17:51:29 +0200

    tinkering with P0.13 pin to cut off 3.3V VCC pin #2

--- zephyr/vcc_power_manager/vcc_power_manager.c
+++ zephyr/vcc_power_manager/vcc_power_manager.c
@@ -1,6 +1,7 @@
 /*
  * VCC Power Manager for nice!nano v2
  * Automatically controls P0.13 to turn off 3.3V VCC when on battery
+ * Works in conjunction with ZMK ext-power system
  */
 
 #include <zephyr/kernel.h>
@@ -15,7 +16,7 @@ LOG_MODULE_REGISTER(vcc_power_manager, CONFIG_ZMK_LOG_LEVEL);
 #ifdef CONFIG_ZMK_VCC_POWER_MANAGER
 
 static const struct gpio_dt_spec vcc_power_pin = GPIO_DT_SPEC_GET_BY_IDX(DT_PATH(zephyr_user), gpios, 0);
-static bool usb_powered = false;
+static bool usb_powered = true; // Assume USB powered at start
 static bool vcc_enabled = true;
 
 static void set_vcc_power(bool enable) {
@@ -26,11 +27,13 @@ static void set_vcc_power(bool enable) {
     
     if (enable && !vcc_enabled) {
         LOG_INF("Enabling VCC power (USB connected)");
+        // P0.13 HIGH enables VCC power
         gpio_pin_set_dt(&vcc_power_pin, 1);
         vcc_enabled = true;
         k_msleep(100); // Give components time to power up
     } else if (!enable && vcc_enabled) {
         LOG_INF("Disabling VCC power (battery mode)");
+        // P0.13 LOW disables VCC power  
         gpio_pin_set_dt(&vcc_power_pin, 0);
         vcc_enabled = false;
     }
@@ -41,6 +44,7 @@ static void usb_status_cb(enum usb_dc_status_code status, const uint8_t *param) 
         case USB_DC_CONNECTED:
         case USB_DC_CONFIGURED:
             if (!usb_powered) {
+                LOG_DBG("USB connected");
                 usb_powered = true;
                 set_vcc_power(true);
             }
@@ -49,6 +53,7 @@ static void usb_status_cb(enum usb_dc_status_code status, const uint8_t *param) 
         case USB_DC_DISCONNECTED:
         case USB_DC_SUSPEND:
             if (usb_powered) {
+                LOG_DBG("USB disconnected");
                 usb_powered = false;
                 set_vcc_power(false);
             }
@@ -67,26 +72,19 @@ static int vcc_power_manager_init(void) {
         return -ENODEV;
     }
     
-    // Configure P0.13 as output
+    // Configure P0.13 as output, initially HIGH (VCC enabled)
     int ret = gpio_pin_configure_dt(&vcc_power_pin, GPIO_OUTPUT_ACTIVE);
     if (ret < 0) {
         LOG_ERR("Failed to configure VCC power GPIO: %d", ret);
         return ret;
     }
     
-    // Initially enable VCC power
+    // Initially enable VCC power (P0.13 = HIGH)
     gpio_pin_set_dt(&vcc_power_pin, 1);
     vcc_enabled = true;
     
     // Register USB status callback
     usb_register_status_callback(usb_status_cb);
-    
-    // Check initial USB state after a delay
-    k_msleep(200);
-    if (!usb_powered) {
-        LOG_INF("No USB detected at startup, disabling VCC power");
-        set_vcc_power(false);
-    }
     
     return 0;
 }
